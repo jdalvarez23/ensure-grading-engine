399e048c248d6d004ae902c3902e4183aa467602
Jose Alvarez <jdalvarez23@gmail.com>
2017-08-25 23:21:05 -0500

    Updated Version 08/25/17

---
+++ src/js/intro.js
@@ -0,0 +1,17 @@
+/**
+ * @fileOverview ensure's library for immediate front-end feedback.
+ * @name intro.js<js>
+ * @author Jose D. Alvarez
+ * @license GPLv3
+ */
+
+/**
+ * Exposes ensureFEGradingEngine (Grading Engine) interface
+ * @return {Object} exports - the functions on the exports object
+ */
+;
+window.ensureFEGradingEngine ? window.ensureFEGradingEngine = window.ensureFEGradingEngine : window.ensureFEGradingEngine = (function(window, undefined) {
+      'use strict';
+      var exports = {};
+
+      // intro.js<js> ends here
---
+++ src/js/outro.js
@@ -0,0 +1,14 @@
+/**
+ * @fileOverview This file contains the closing statements of the Grading Engine.
+ * @name outro.js<js>
+ * @author Jose D. Alvarez
+ * @license GPLv3
+ */
+/* jshint ignore:start */
+return exports;
+}(window));
+window.dispatchEvent(new CustomEvent('GE-on', {detail: true}));
+/* jshint ignore:end */
+
+// outro<js>.js ends here
+// GE.js ends here
